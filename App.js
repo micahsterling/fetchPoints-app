b2cf99f8f08224195d4c7e1a35096864677deb58
Micah Sterling <msterling89@gmail.com>
2022-10-17 14:36:21 -0500

    add server/requirements

---
+++ App.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+
+//Use bodyParser
+app.use(
+  bodyParser.urlencoded({ extended: true })
+);
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+const transaction = []
+
+app.listen(4000, function () {
+  console.log("Started application on port %d", 4000)
+});
